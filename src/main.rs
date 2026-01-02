4616630f0f7494898d33b8fd75aa1a1a4e0a1b3e
lambert <leandro.salustriano@hotmail.com>
2026-01-01 23:51:35 -0300

    Refactor `main.rs` to simplify `main` function with a single `scan` implementation.

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,8 @@
-extern crate url;
-
-use futures::future::join_all;
-use std::env;
-use std::io::{self, Read};
 use url::Url;
-use wappalyzer::scan;
 
-fn main() {
+#[tokio::main]
+async fn main() {
+    let url = Url::parse(&String::from("https://200.150.197.45:443")).expect("ERR");
+    let res = wappalyzer::scan(url, Option::from(true)).await;
+    println!("{:?}", res);
 }
-
-// #[tokio::main]
-// async fn main() -> Result<(), Box<dyn std::error::Error>> {
-//     let args: Vec<String> = env::args().collect();
-//
-//     let mut urls = vec![];
-//     if args.len() == 1 {
-//         let mut buffer = String::new();
-//         io::stdin().read_to_string(&mut buffer)?;
-//         urls.extend(strings_to_urls(buffer));
-//     } else {
-//         urls.push(Url::parse(&String::from(&args[1]))?);
-//     }
-//
-//     let futures = urls.into_iter().map(scan).collect::<Vec<_>>();
-//     let results = join_all(futures).await;
-//     for res in results {
-//         if let Ok(output) = serde_json::to_string(&res) {
-//             println!("{}", output);
-//         }
-//     }
-//     Ok(())
-// }
-//
-// fn strings_to_urls(domains: String) -> Vec<Url> {
-//     domains
-//         .split_terminator('\n')
-//         .map(|s| Url::parse(s))
-//         .filter_map(Result::ok)
-//         .collect()
-// }
