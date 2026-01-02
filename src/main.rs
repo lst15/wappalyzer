cff03942167cc8cf39e6d4de665ca505afc74088
lambert <leandro.salustriano@hotmail.com>
2026-01-01 22:00:59 -0300

    Commented out the main async logic and related helper function in `main.rs`.

--- src/main.rs
+++ src/main.rs
@@ -6,33 +6,36 @@ use std::io::{self, Read};
 use url::Url;
 use wappalyzer::scan;
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let args: Vec<String> = env::args().collect();
-
-    let mut urls = vec![];
-    if args.len() == 1 {
-        let mut buffer = String::new();
-        io::stdin().read_to_string(&mut buffer)?;
-        urls.extend(strings_to_urls(buffer));
-    } else {
-        urls.push(Url::parse(&String::from(&args[1]))?);
-    }
-
-    let futures = urls.into_iter().map(scan).collect::<Vec<_>>();
-    let results = join_all(futures).await;
-    for res in results {
-        if let Ok(output) = serde_json::to_string(&res) {
-            println!("{}", output);
-        }
-    }
-    Ok(())
+fn main() {
 }
 
-fn strings_to_urls(domains: String) -> Vec<Url> {
-    domains
-        .split_terminator('\n')
-        .map(|s| Url::parse(s))
-        .filter_map(Result::ok)
-        .collect()
-}
+// #[tokio::main]
+// async fn main() -> Result<(), Box<dyn std::error::Error>> {
+//     let args: Vec<String> = env::args().collect();
+//
+//     let mut urls = vec![];
+//     if args.len() == 1 {
+//         let mut buffer = String::new();
+//         io::stdin().read_to_string(&mut buffer)?;
+//         urls.extend(strings_to_urls(buffer));
+//     } else {
+//         urls.push(Url::parse(&String::from(&args[1]))?);
+//     }
+//
+//     let futures = urls.into_iter().map(scan).collect::<Vec<_>>();
+//     let results = join_all(futures).await;
+//     for res in results {
+//         if let Ok(output) = serde_json::to_string(&res) {
+//             println!("{}", output);
+//         }
+//     }
+//     Ok(())
+// }
+//
+// fn strings_to_urls(domains: String) -> Vec<Url> {
+//     domains
+//         .split_terminator('\n')
+//         .map(|s| Url::parse(s))
+//         .filter_map(Result::ok)
+//         .collect()
+// }
